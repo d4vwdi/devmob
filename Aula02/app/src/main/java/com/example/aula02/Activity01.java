a6e76a7b0a7088d943d83710b23abdc4660bcc0c
d4vwdi <d4vwdi@gmail.com>
2024-03-06 11:15:17 -0300

    Inserindo os campos e mostrando o resultado no log

--- Aula02/app/src/main/java/com/example/aula02/Activity01.java
+++ Aula02/app/src/main/java/com/example/aula02/Activity01.java
@@ -1,5 +1,8 @@
 package com.example.aula02;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -8,10 +11,25 @@ public class Activity01 extends AppCompatActivity {
     public void onCreate(Bundle bundle){
         super.onCreate(bundle);
 
+
+
+
         TextView txt = new TextView(this);
         txt.setText(R.string.hello);
         setContentView(txt);
 
         setContentView(R.layout.layout_activity01);
-    }
+
+        // Chamada dos dados inseridos pelo usuário
+    EditText edt_nome = findViewById(R.id.edt_nome);
+    EditText edt_email = findViewById(R.id.edt_email);
+    EditText edt_telefone = findViewById(R.id.edt_telefone);
+
+    Button btn_gravar = findViewById(R.id.btn_gravar);
+    btn_gravar.setOnClickListener( e -> {
+        Log.i("AGENDA_CONTATO", "Nome: " + edt_nome.getText());
+        Log.i("AGENDA_CONTATO", "E-mail: " + edt_email.getText());
+        Log.i("AGENDA_CONTATO", "Telefone: " + edt_telefone.getText());
+    });
+}
 }
